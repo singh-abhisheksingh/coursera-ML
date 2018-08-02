7c35b0b56b367ffc3343369aabc2e83ff96d1d71
singh-abhisheksingh <abhisheksingh0058@gmail.com>
2018-08-03 01:02:04 +0530

    Calculating the cost

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -62,25 +62,47 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+a1 = [ones(m, 1) X];
+z2 = a1*Theta1';
+a2 = [ones(size(sigmoid(z2), 1), 1) sigmoid(z2)];
+z3 = a2*Theta2';
+a3 = sigmoid(z3);
+hx = a3;
 
+y_new = zeros(m, num_labels);
 
+for i=1:m
+	y_new(i, y(i)) = 1;
+end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+J = 1/m * sum(sum( (-y_new .* log(hx)) - ((1-y_new) .* log(1-hx)) ));
+J = J + lambda/(2*m) * ( sum(sum(Theta1(:, 2:end) .^ 2)) + sum(sum(Theta2(:, 2:end) .^ 2)) );
 
 % -------------------------------------------------------------
+
+for t = 1:m
+
+	a1 = X(t, :);
+	a1 = [1 a1];
+	z2 = a1*Theta1';
+	a2 = [1 sigmoid(z2)];
+	z3 = a2*Theta2';
+	a3 = sigmoid(z3);
+	
+	del3 = a3 - y_new(t, :);
+	del2 = (del3 * Theta2) .* sigmoidGradient([1 z2]);
+	del2 = del2(2:end);
+	
+	Theta2_grad = Theta2_grad + del3'*a2;
+	Theta1_grad = Theta1_grad + del2'*a1;
+
+end
+
+Theta2_grad = (1/m) * Theta2_grad;
+Theta1_grad = (1/m) * Theta1_grad;
+
+Theta2_grad(:, 2:end) = Theta2_grad(:, 2:end) + (lambda/m) * (Theta2(:, 2:end));
+Theta1_grad(:, 2:end) = Theta1_grad(:, 2:end) + (lambda/m) * (Theta1(:, 2:end));
 
 % =========================================================================
 
